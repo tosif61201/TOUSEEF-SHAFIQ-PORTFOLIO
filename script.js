ce9ce116db39e40ee350ec9a2bc14cfc917d697c
Touseef <myaim1241@gmail.com>
2024-01-20 02:02:42 +0500

    Back Profile Button

--- script.js
+++ script.js
@@ -21,3 +21,49 @@ pageTurnBtn.forEach((el, index)=>{
         }
     }
 })
+
+//Contect Me Button
+const pages = document.querySelectorAll('.book-page.page-right')
+const contectMeBtn = document.querySelector('.btn.contect-me')
+
+contectMeBtn.onclick = ()=>{
+    pages.forEach((page, index)=>{
+        setTimeout(()=>{
+            page.classList.add('turn');
+
+            setTimeout(()=>{
+                page.style.zIndex = 200 + index;
+            }, 500)
+        }, (index + 1) * 200 + 100)
+    })
+}
+
+//Reverse Index Function
+let totalPages = pages.length;
+let pageNumber = 0;
+
+function reverseIndex() {
+    pageNumber--;
+    if(pageNumber < 0){
+        pageNumber = totalPages -1;
+    }
+}
+
+//back Profile Button
+const backProfileBtn = document.querySelector('.back-profile');
+
+backProfileBtn.onclick = () =>{
+    pages.forEach((_, index)=>{
+        setTimeout(()=>
+        {
+            reverseIndex();
+            pages[pageNumber].classList.remove('turn');
+
+        setTimeout(()=>{
+            reverseIndex();
+            pages[pageNumber].style.zIndex = 10 + index;
+        }, 500)
+
+    },(index + 1) * 200 + 100 )
+    })
+}
