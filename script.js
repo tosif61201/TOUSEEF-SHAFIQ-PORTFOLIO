987ae6526aed44adb197654f2ce7fa8c3f5f623c
Touseef <myaim1241@gmail.com>
2024-01-20 00:41:28 +0500

    Next & Previous Button Functional

---
+++ script.js
@@ -0,0 +1,23 @@
+// tun page on clicking btn
+const pageTurnBtn = document.querySelectorAll('.nextprev-btn');
+
+pageTurnBtn.forEach((el, index)=>{
+    el.onclick =()=>{
+        const pageTurnId = el.getAttribute('data-page')
+        const pageTurn = document.getElementById(pageTurnId);
+
+        if(pageTurn.classList.contains('turn')){
+            pageTurn.classList.remove('turn');
+            setTimeout(()=>{
+                pageTurn.style.zIndex = 20 - index;
+            }, 500);
+        }
+
+        else{
+            pageTurn.classList.add('turn');
+            setTimeout(()=>{
+                pageTurn.style.zIndex = 20 + index;
+            }, 500);
+        }
+    }
+})
